29c3f5f81e15cec712627190a16e5207459e4236
eliranmal <eliranmal@gmail.com>
2017-12-29 17:29:34 +0200

    add pack script

---
+++ bin/pack.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+main() {
+
+	local source_dir="$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )"
+	local root_dir="$source_dir"'/..'
+	local pack_source_dir="$root_dir"'/app'
+	local pack_target_dir="$root_dir"'/export'
+
+	echo "packing [$pack_source_dir => $pack_target_dir]"
+	
+	pushd "$pack_source_dir" \
+	    && zip -v "$pack_target_dir"'/octosort.zip' -r * \
+	    && popd
+}
+
+main
