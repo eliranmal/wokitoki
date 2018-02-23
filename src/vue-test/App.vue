dd2129eb1f40ef77867148ba8d7043e285758443
eliranmal <eliranmal@gmail.com>
2018-02-23 17:18:57 +0200

    storage sync stuff

--- src/vue-test/App.vue
+++ src/vue-test/App.vue
@@ -1,23 +1,21 @@
 <template>
     <div class="app">
-        <div v-show="isLoading" class="loader centered-content">
-            <i class="fa fa-cog fa-spin fa-2x"></i>
-        </div>
-
-        <!--fixme - no need to set room on both-->
-        <room v-if="roomName" v-bind:room-name="roomName" v-on:leave="setRoom"/>
-        <welcome v-else v-on:enter="setRoom"/>
+        <loader v-bind:is-loading="isLoading" />
+        <room v-if="roomName" v-bind:room-name="roomName" v-on:leave="leave"/>
+        <welcome v-else v-on:enter="enter"/>
     </div>
 </template>
 
 <script>
     import storage from '../lib/storage';
+    import Loader from './components/Loader';
     import Welcome from './components/Welcome';
     import Room from './components/Room';
 
     export default {
         name: 'app',
         components: {
+            Loader,
             Welcome,
             Room,
         },
@@ -25,32 +23,46 @@
             return {
                 roomName: null,
                 isLoading: true,
+                showRoom: false,
             };
         },
-        methods: {
-            setRoom(roomName) {
-                console.log('setting room name to ', roomName);
-                this.isLoading = true;
-                this.roomName = roomName;
-                if (roomName) {
-                    storage.set({roomName: roomName}, () => {
-                        console.log('saved room name to storage');
-                        this.isLoading = false;
-                    });
-                } else {
-                    this.isLoading = false;
-                }
-            },
-        },
         mounted() {
-            this.isLoading = true;
-            storage.get('roomName', (name) => {
-                console.log('got room name from storage:', name);
-                if (name) {
-                    this.roomName = name;
+            this.retrieveRoom();
+        },
+        methods: {
+            enter(roomName) {
+                if (!roomName) {
+                    return;
                 }
-                this.isLoading = false;
-            });
+                this.saveRoom(roomName);
+            },
+            leave() {
+                this.clearRoom();
+            },
+            saveRoom(name) {
+                this.isLoading = true;
+                storage.set({roomName: name}, () => {
+                    console.log('saved room name to storage', name);
+                    this.roomName = name;
+                    this.isLoading = false;
+                });
+            },
+            retrieveRoom() {
+                this.isLoading = true;
+                storage.get('roomName', (name) => {
+                    console.log('got room name from storage:', name);
+                    this.roomName = name;
+                    this.isLoading = false;
+                });
+            },
+            clearRoom() {
+                this.isLoading = true;
+                storage.remove('roomName', () => {
+                    console.log('removed room name from storage');
+                    this.roomName = null;
+                    this.isLoading = false;
+                });
+            },
         },
     };
 </script>
@@ -58,21 +70,12 @@
 <style>
 
     .app {
-        display: flex;
-        justify-content: center;
+        /* todo - i'm not sure i like this effect. decide what to do here */
+        /*display: flex;*/
+        /*justify-content: center;*/
         overflow: hidden;
         min-height: 100vh;
         padding: 4rem;
-    }
-
-    .loader {
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        z-index: 1;
-        background-color: #f6f6f6;
     }
 
 </style>
