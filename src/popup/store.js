f6f4813043ce1d14eadc4ee1698cccdf86e4cdc8
eliranmal <eliranmal@gmail.com>
2018-03-12 01:16:18 +0200

    encapsulate namespace operations, protect from commit on undefined values

--- src/popup/store.js
+++ src/popup/store.js
@@ -1,6 +1,7 @@
 // todo - make this into a store factory, extract all store sections into their own modules
 
 import storage from '../lib/storage';
+import namespace from '../lib/namespace';
 
 
 const debug = process.env.NODE_ENV === 'development';
@@ -22,44 +23,43 @@ const defaults = Object.freeze(Object.assign({}, state));
 const mutations = {
     update(state, payload) {
         for (let [key, value] of Object.entries(payload)) {
-            trace(`> vuex store > mutation > key: '${key}', value: '${value}'`);
-            if (key.includes('.')) {
-                const seg = key.split('.');
-                // naive implementation - few use-cases
-                switch (seg.length) {
-                    case 2:
-                        state[seg[0]][seg[1]] = value;
-                        break;
-                    case 3:
-                        state[seg[0]][seg[1]][seg[2]] = value;
-                        break;
-                }
-                return;
+            trace(`> store > mutation > key: '${key}', value: '${value}'`);
+            const oldValue = namespace.get(key, state);
+            if (namespace.get(key, state) !== value) {
+                namespace.set(key, value, state);
+                trace(`> store > mutation done > key: '${key}', old value: '${oldValue}', new value: '${namespace.get(key, state)}'`);
             }
-            state[key] = value;
         }
     },
 };
 
 const actions = {
     retrieve({commit}, {key, commit: doCommit = true, done = () => 1}) {
-        trace(`> vuex store > action [retrieve] > key: '${key}'.`);
-        storage.get({[key]: defaults[key]}, (data) => {
-            doCommit && commit('update', {[key]: data[key]});
+        trace(`> store > action [retrieve] > key: '${key}'.`);
+        const defaultValue = namespace.get(key, defaults);
+        storage.get({[key]: defaultValue}, (data) => {
+            trace(`> store > action [retrieve] done > response:`, data);
+            // chrome sync storage might return undefined on errors. avoid the commit if so
+            if (doCommit && typeof data[key] !== 'undefined') {
+                commit('update', {[key]: data[key]});
+            }
             done();
         });
     },
     save({commit}, {key, value, commit: doCommit = true, done = () => 1}) {
-        trace(`> vuex store > action [save] > key: '${key}', value: '${value}'.`);
+        trace(`> store > action [save] > key: '${key}', value: '${value}'.`);
         storage.set({[key]: value}, () => {
+            trace(`> store > action [save] done > key: '${key}', value: '${value}'`);
             doCommit && commit('update', {[key]: value});
             done();
         });
     },
     clear({commit}, {key, commit: doCommit = true, done = () => 1}) {
-        trace(`> vuex store > action [clear] > key: '${key}'.`);
+        trace(`> store > action [clear] > key: '${key}'.`);
+        const defaultValue = namespace.get(key, defaults);
         storage.remove(key, () => {
-            doCommit && commit('update', {[key]: defaults[key]});
+            trace(`> store > action [clear] done > key: '${key}'`);
+            doCommit && commit('update', {[key]: defaultValue});
             done();
         });
     },
